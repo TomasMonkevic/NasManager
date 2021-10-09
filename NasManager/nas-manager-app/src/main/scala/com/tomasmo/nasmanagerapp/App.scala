4e353710b41e46ff1eed922e59cdd8ba8dd62619
Tomas Monkevic <tomas.monkevic97@gmail.com>
2021-10-09 16:54:03 +0300

    Init maven

---
+++ NasManager/nas-manager-app/src/main/scala/com/tomasmo/nasmanagerapp/App.scala
@@ -0,0 +1,12 @@
+package com.tomasmo.nasmanagerapp
+
+import com.tomasmo.nasmanagercore.Foo
+
+/**
+ * Hello world!
+ *
+ */
+object NasApp extends App {
+  println( "Hello World!" )
+  println(Foo.add(2, 4))
+}
---
+++ NasManager/nas-manager-app/src/main/scala/com/tomasmo/nasmanagercore/Foo.scala
@@ -0,0 +1,5 @@
+package com.tomasmo.nasmanagercore
+
+object Foo {
+  def add(a: Int, b: Int): Int = a + b
+}
